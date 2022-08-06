acff288f71990fc663d26f23fe8a49923f850785
Muhtasim Fuad Ayon <92244527+MuhtasimFuad069@users.noreply.github.com>
2022-08-06 22:02:14 +0600

    Add files via upload

---
+++ update appointment list.php
@@ -0,0 +1,15 @@
+<?php
+$link = mysqli_connect("localhost", "root", "", " Smart Health Prediction "); 
+  
+if($link === false){ 
+    die("ERROR: Could not connect. " . mysqli_connect_error()); 
+} 
+  
+$sql = "UPDATE appointments list SET Date='250' WHERE patient_ID=11111"; 
+if(mysqli_query($link, $sql)){ 
+    echo "Record was updated successfully."; 
+} else { 
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link); 
+}  
+mysqli_close($link); 
+?> 
